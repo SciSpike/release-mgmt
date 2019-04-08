6bec416f2e4b2e9edb48e2bf7245ed52b5ce7dc8
Matthew Adams <matthew@matthewadams.me>
2019-04-08 11:20:52 -0500

    rm image-codefresh from test-all.sh

--- test/test-all.sh
+++ test/test-all.sh
@@ -2,7 +2,7 @@
 set -ex
 
 THIS_ABSPATH="$(cd "$(dirname "$0")"; pwd)"
-TYPES=${@:-nodejs chart version image image-codefresh}
+TYPES=${@:-nodejs chart version image}
 
 for type in $TYPES; do
   export TEST_DOCKER=
@@ -11,7 +11,5 @@ for type in $TYPES; do
   "$THIS_ABSPATH/test.sh" $type
 
   export TEST_DOCKER=1
-  export NO_USE_LOCAL_FX=
-  export NO_USE_LOCAL_MATCH=
   "$THIS_ABSPATH/test.sh" $type
 done
