6fc9fc1450f4771599a2a0ce07eb8670059a6d49
Brent Fleckner <brent.fleckner@scispike.com>
2019-04-23 09:01:08 -0700

    wip

---
+++ test/net/local-src/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyCompany("iGT")]
+[assembly: AssemblyProduct("iGT PaaS")]
+[assembly: AssemblyCopyright("Copyright © 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyInformationalVersion("2.0.0-pre.0")]
+[assembly: AssemblyVersion("2.0.0.*")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+
--- test/test-all.sh
+++ test/test-all.sh
@@ -2,13 +2,13 @@
 set -ex
 
 THIS_ABSPATH="$(cd "$(dirname "$0")"; pwd)"
-TYPES=${@:-nodejs chart version image nodejs+image}
+TYPES=${@:-nodejs chart version image nodejs+image net}
 
 for type in $TYPES; do
-  export TEST_DOCKER=
-  export NO_USE_LOCAL_FX=1
-  export NO_USE_LOCAL_MATCH=1
-  "$THIS_ABSPATH/test.sh" "$type"
+  #export TEST_DOCKER=
+  #export NO_USE_LOCAL_FX=1
+  #export NO_USE_LOCAL_MATCH=1
+  #"$THIS_ABSPATH/test.sh" "$type"
 
   export TEST_DOCKER=1
   "$THIS_ABSPATH/test.sh" "$type"
